fb6c8826b06f93f6e99ee14f08de793461b57510
Jonas <Saltogpeber123@hotmail.com>
2019-03-26 00:50:28 +0100

    Upload assets + added folders + simple player functionality

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 target;
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        animator.SetBool("isMoving", true);
+        }
+        
+        transform.position = Vector2.MoveTowards(this.transform.position, target, 0.02f);
+        
+        }
+
+    }
+
